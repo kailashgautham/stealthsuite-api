0f24f8d0b394c8d9f49f9b093b1204d61302f719
kailashgautham <kailash.gautham@gmail.com>
2024-04-07 21:38:41 +0800

    add env port num

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ const db = knex({
     },
 });
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 })
 
 app.get('/', (req, res) => {
