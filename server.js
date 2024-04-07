7e80b20fb08bed834b6d473db378970d148069c6
kailashgautham <kailash.gautham@gmail.com>
2024-04-07 22:46:54 +0800

    add db

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+
 const db = knex({
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: 'kailashgautham',
-      password: '',
-      database: 'stealth-suite',
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     },
 });
 
